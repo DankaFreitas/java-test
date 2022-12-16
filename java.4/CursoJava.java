60ddc2ace4914d8cde60a7b2dcdd64b88866112c
Oberdã <carlos-oberda@hotmail.com>
2022-12-16 10:56:09 -0300

    add java4

---
+++ java.4/CursoJava.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class CursoJava {
+    public static void main(String [] args){
+        Scanner scan = new Scanner(System.in);
+        int n1=0,n2=0,n3=0,n4=0,soma=0;
+        final int MEDIA=60;
+        String nome="",res="";
+
+
+        System.out.println("Digite seu nome do aluno: ");
+        nome=scan.nextLine();
+        System.out.println("Digite a primeira nota: ");
+        n1=scan.nextInt();
+        System.out.println("Digite a segunda nota: ");
+        n2=scan.nextInt();
+        System.out.println("Digite a terceira nota: ");
+        n3=scan.nextInt();
+        System.out.println("Digite a quarta nota: ");
+        n4=scan.nextInt();
+        soma=n1+n2+n3+n4;
+        if(soma >= MEDIA){
+            res="Aprovado";
+        }else if(soma >= 40){
+            res="Recuperacao";
+        }else{
+            res="Reprovado";
+        }
+        System.out.printf("Aluno %s foi %s, com nota %d" ,nome,res,soma);
+
+    }
+}

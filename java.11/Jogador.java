3febebfa6d7d21f17fa4c0514d174bdb82af0da8
Oberdã <carlos-oberda@hotmail.com>
2022-12-24 12:29:13 -0300

    Diferença entre public e private

---
+++ java.11/Jogador.java
@@ -0,0 +1,11 @@
+public class Jogador {
+    
+    private int num =0;
+    
+    
+    public Jogador(int num){
+        this.num=num;
+        System.out.printf("%nJogador numero %d criado%n",num);
+    }
+    
+}

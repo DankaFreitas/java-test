5ef7f6250756bedd67bc7069038e37a0bb79ce32
Oberdã <112282369+DankaFreitas@users.noreply.github.com>
2023-01-20 09:15:03 -0300

    Update CursoJava.java

--- java.19/CursoJava.java
+++ java.19/CursoJava.java
@@ -1,6 +1,7 @@
 public class CursoJava {
     public static void main(String[] args) {
         
-        Animal n1=new Animal();
+        //Animal n1=new Animal();
+        Formiga a1=new Formiga();
     }
 }

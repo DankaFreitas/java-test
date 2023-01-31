1018c66fe4de7d7d5bab7ba56a52e00b27561f33
Oberdã <carlos-oberda@hotmail.com>
2023-01-31 10:02:13 -0300

    manipular Array em Java

---
+++ java.24/CursoJava.java
@@ -0,0 +1,27 @@
+public class CursoJava {
+    public static void main(String[] args) {
+    
+        
+        //final int tamanho=5;
+        //int[] num=new int[tamanho];
+
+        int[] num={52,100,3,15,527};
+     
+        /* 
+        num[0]=52;
+        num[1]=100;
+        num[2]=3;
+        num[3]=15;
+        num[4]=527;
+        
+        for(int i=0;i<tamanho;i++){
+            System.out.printf("%d%n", num[i]);
+        }
+         */
+
+        for(int n:num){
+            System.out.printf("%d%n", n);
+        }
+    
+    }
+ }
